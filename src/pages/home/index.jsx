a8ac9fc9ab95adfc12bac03cda2e3e68982af0c8
Liliana <lily.gallegos@hotmail.com>
2021-12-30 10:42:30 -0600

    principal layout

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,9 +1,27 @@
 import React from "react";
+import im1 from "../../img/good-business-talk-2021-09-01-10-50-33-utc.png";
+import "./css/style.css";
 
 const Home = () =>{
     return(
         <>
-        <h1>Home</h1>
+        <div className="main-Container">
+            <div className="description-left">
+                <h1>Crece tu negocio con la mejor plataforma de Facturación en Línea y Servicios Contables</h1>
+                <p>Hacemos tu contabilidad y te ofrecemos el sistema mas facil de usar, ideal para Pymes y Emprendedores.</p>
+            </div>
+            <div>
+                <img src={im1} alt="im1"></img>
+            </div>
+        </div>
+        <div className="main-Container">
+        <div className="image">
+                <img src={im1} alt="im1"></img>
+            </div>
+            <div className="description-right">
+                <h1>Hola desde home lado izq</h1>
+            </div>
+        </div>
         </>
     )
 }
