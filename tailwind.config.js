904232e4adeb9ac1aef8079e267b5c153d8d3ba7
Liliana <lily.gallegos@hotmail.com>
2021-12-30 11:04:47 -0600

    tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    colors: {
+      'blue': '#0D66CE',
+      'green': '#2AEAA5',
+    },
+    extend: {},
+  },
+  plugins: [],
+};
