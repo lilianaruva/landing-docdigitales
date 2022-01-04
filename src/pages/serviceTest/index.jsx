b984b3196c08134e790080f7bbb0f42f113dd1fa
Martín Valenzuela <martynvalenz@gmail.com>
2022-01-04 00:53:25 -0700

    service test card

--- src/pages/serviceTest/index.jsx
+++ src/pages/serviceTest/index.jsx
@@ -1,11 +1,18 @@
 import React from "react";
+import logo from "../../img/home/DD_Logo.svg"
+import './style.css'
 
 const ServiceTest = () => {
   return (
     <>
-			<div className="flex h-screen">
+			<div className="flex h-screen service-background-color">
 				<div className="m-auto">
-					<div class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex">
+					<div className="flex py-[30px]">
+						<div className="m-auto">
+							<img src={logo} className="self-center" style={{ width:"200px"}} alt="DD_Logo" />
+						</div>
+					</div>
+					<div class="shadow-[0px_3px_20px_#B9CCEF80] px-8 pt-6 pb-8 mb-4 flex" style={{ background:'white' }}>
 						<div class="mb-4">
 							<label class="block text-grey-darker text-sm font-bold mb-2" for="username">
 								Username
