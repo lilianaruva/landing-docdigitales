8f0226050e05df16b8ee3df51c423185eac729a6
Liliana <lily.gallegos@hotmail.com>
2022-01-05 11:40:36 -0600

    fix store

--- src/store.js
+++ src/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit'
-import articlesReducer from './redux/reducers/Articles';
+import articlesReducer from './redux/reducers/articles';
 
 export const store = configureStore({
   reducer: {
