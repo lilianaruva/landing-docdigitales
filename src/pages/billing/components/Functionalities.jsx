f08714fff343709e590efdde6ebc86a33f924647
Martín Valenzuela <martynvalenz@gmail.com>
2022-01-06 17:29:26 -0700

    Update Functionalities.jsx

--- src/pages/billing/components/Functionalities.jsx
+++ src/pages/billing/components/Functionalities.jsx
@@ -62,9 +62,23 @@ const Functionalities = () => {
             </div>
           </div>
         </div> */}
-        {/* <div className="flex">
-          Facturación
-        </div> */}
+        <ul className="flex flex-wrap border-b border-gray-200 dark:border-gray-700">
+          <li className="mr-2">
+              <a href="#" aria-current="page" className="inline-block bg-gray-100 text-blue-600 rounded-t-lg py-4 px-4 text-sm font-medium text-center active dark:bg-gray-800 dark:text-blue-500">Profile</a>
+          </li>
+          <li className="mr-2">
+              <a href="#" className="inline-block text-gray-500 hover:text-gray-600 hover:bg-gray-50 rounded-t-lg py-4 px-4 text-sm font-medium text-center dark:text-gray-400  dark:hover:bg-gray-800 dark:hover:text-gray-300">Dashboard</a>
+          </li>
+          <li className="mr-2">
+              <a href="#" className="inline-block text-gray-500 hover:text-gray-600 hover:bg-gray-50 rounded-t-lg py-4 px-4 text-sm font-medium text-center dark:text-gray-400 dark:hover:bg-gray-800 dark:hover:text-gray-300">Settings</a>
+          </li>
+          <li className="mr-2">
+              <a href="#" className="inline-block text-gray-500 hover:text-gray-600 hover:bg-gray-50 rounded-t-lg py-4 px-4 text-sm font-medium text-center dark:text-gray-400  dark:hover:bg-gray-800 dark:hover:text-gray-300">Contacts</a>
+          </li>
+          <li>
+              <a className="inline-block text-gray-400 rounded-t-lg py-4 px-4 text-sm font-medium text-center dark:text-gray-500 cursor-not-allowed">Disabled</a>
+          </li>
+      </ul>
         <div className="flex items-center">
           <div className="lg:w-1/2 content-center lg:mx-[80px]">
             <b className="text-[35px]">Facturación</b>
