2ffea40938ef8ee953fe326472c72a14814191a2
Liliana <lily.gallegos@hotmail.com>
2022-01-11 11:23:51 -0600

    article 07

--- src/pages/company/resources/components/articlePage.jsx
+++ src/pages/company/resources/components/articlePage.jsx
@@ -6,13 +6,14 @@ import Article03 from "./articles/03";
 import Article04 from "./articles/04";
 import Article05 from "./articles/05";
 import Article06 from "./articles/06";
+import Article07 from "./articles/07";
 import '../resources.css'
 
 const ArticlePage = ({ }) => {
     return (
         <>
             <div className="main-ContainerArticle" style={{ marginTop: '134px' }}>
-                <Article06 />
+                <Article07 />
             </div>
             <div className="mainMedium-Container" style={{ borderTop: '2px solid #DDE6F699' }}>
                 <div className="title-platform">
