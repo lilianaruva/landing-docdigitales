d3280280ed793fc7b53a62a16175c1b55924d6d3
Liliana <lily.gallegos@hotmail.com>
2022-01-11 19:04:20 -0600

    ready section

--- src/pages/home/components/ready.jsx
+++ src/pages/home/components/ready.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import calendar from "../../../img/home/calendar-blue.png";
 import card from "../../../img/home/card-blue.png"
+import { Link } from "react-router-dom";
 
 const Ready = () => {
     return (
         <>
-            <div className="mainMedium-Container" style={{ backgroundColor: '#0063CE', color: '#FFFFFF' }}>
+            <div className="mainMedium-Container" id="pointsBackground" style={{ backgroundColor: '#0063CE', color: '#FFFFFF' }}>
                 <div className="titleAboutUs" style={{ textAlign: 'center' }}>
                     <p style={{ fontWeight: '500', marginBottom: '1rem' }}>¿Listo para probar nuestros servicios?</p>
                     <p style={{ fontSize: '25px' }}>Prueba hoy la solución de facturación y contabilidad más sencilla de México</p>
@@ -25,8 +26,11 @@ const Ready = () => {
                     </div>
                 </div>
                 <div className="alreadyBottomBtn">
-                    <button>Solicitar prueba</button>
+                    <Link to="/prueba-de-servicio">
+                        <button>Solicitar prueba</button>
+                    </Link>
                 </div>
+
             </div>
         </>
     )
