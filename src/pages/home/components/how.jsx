7280bcfce7accad0ec9b0a082c5e86480c07accd
alonso16 <alonso@itnora.com>
2022-01-14 15:57:40 -0600

    Update re design

--- src/pages/home/components/how.jsx
+++ src/pages/home/components/how.jsx
@@ -1,7 +1,9 @@
 import React from "react";
+/*
 import imgScreen from "../../../img/home/Screen Shot 2021-10-08 at 5.17.18 PM.png";
 import imgLeft from "../../../img/home/[Downloader.la]-61ba510681cc0.png"
 import imgRight from "../../../img/home/shutterstock_404098204.png"
+*/
 
 const How = () => {
     return (
@@ -11,7 +13,18 @@ const How = () => {
                     <p>¿Cómo <b>nos integramos</b> a tu negocio?</p>
                     <p style={{ fontSize: '30px' }}>Trabajamos para ti, llevamos tu contabilidad y te brindamos las herramientas de administración en línea más fáciles de usar.</p>
                 </div>
-                <div className="howTree">
+            </div>
+        </>
+    )
+}
+
+export default How;
+
+
+/*
+
+
+ <div className="howTree">
                     <div className="howLeft">
                         <div className="howTop">
                             <img src={imgLeft} alt="howImgLeft"/>
@@ -37,9 +50,5 @@ const How = () => {
                         <div className="howItem">Lleva el control de tu negocio</div>
                     </div>
                 </div>
-            </div>
-        </>
-    )
-}
 
-export default How;
+*/
