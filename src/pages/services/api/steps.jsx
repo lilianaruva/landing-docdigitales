7cbcb8f62dcded3e3ff33860826429a78dac184e
lilianaruva <lily.gallegos@hotmail.com>
2022-01-18 14:02:57 -0600

    clean code

--- src/pages/services/api/steps.jsx
+++ src/pages/services/api/steps.jsx
@@ -1,23 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 import step1 from "../../../img/services/api/Group 1487.svg"
 import step2 from "../../../img/services/api/Group 1488.svg"
 import step3 from "../../../img/services/api/Group 1489.svg"
 import step4 from "../../../img/services/api/Group 1490.svg"
 import step5 from "../../../img/services/api/Group 1491.svg"
 
-//Animations
-import { motion } from "framer-motion";
-import { pageAnimation } from "../../../animation";
-
 
 const Steps = () => {
     return (
         <>
-            <motion.div className="mainMedium-Container"
-                exit="exit"
-                variants={pageAnimation}
-                initial="hidden"
-                animate="show">
+            <div className="mainMedium-Container">
                 <div className="title-platform">
                     <p style={{ fontWeight: '500' }}>¿Cuáles son los pasos para <b>integrarte a docDigitales API?</b></p>
                 </div>
@@ -48,7 +40,7 @@ const Steps = () => {
                         <p style={{ width: '100px', fontWeight: '500' }}>Contrata</p>
                     </div>
                 </div>
-            </motion.div>
+            </div>
         </>
     )
 }
