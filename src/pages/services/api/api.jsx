5341a3dc8794f95ac220137c3010f2c8727886fa
lilianaruva <lily.gallegos@hotmail.com>
2022-01-17 18:55:33 -0600

    api resources

--- src/pages/services/api/api.jsx
+++ src/pages/services/api/api.jsx
@@ -1,4 +1,7 @@
 import React, { useEffect } from 'react';
+import im1 from '../../../img/resources/Laptop 04.png';
+import pointBlue from "../../../img/pointPurple.svg";
+import './api.css';
 
 const Api = () => {
 
@@ -7,9 +10,21 @@ const Api = () => {
     }, [])
 
 
-    return(
+    return (
         <>
-         <h1>Apia</h1>
+            <div className="Principal-main-Container" id="backgroundApi" style={{ paddingTop: '100px', backgroundColor: '#F1F5FE' }}>
+                <div className="description-left" style={{ marginRight: '20px' }}>
+                    <div className="titleAboutUs">
+                        <p><b>Api</b> de facturación en línea</p>
+                        <h3 style={{ fontSize: '25px', marginTop: '20px' }} className="titleResponsive">DocDigitales API es una gran herramienta para integrar la facturación electrónica a tu sistema.</h3>
+                    </div>
+                    <button className="btnResourcesPurple">Solicitar prueba</button>
+                </div>
+                <div className="description-right">
+                    <img src={im1} alt="im1"></img>
+                    <img src={pointBlue} alt="pointBlue" id="pointRightBottom"></img>
+                </div>
+            </div>
         </>
     )
 }
