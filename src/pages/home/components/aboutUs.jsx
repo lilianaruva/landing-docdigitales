96f3eebbd63c5ff711a90abfcc1c791003a310e4
lilianaruva <lily.gallegos@hotmail.com>
2022-01-18 18:22:37 -0600

    link to services

--- src/pages/home/components/aboutUs.jsx
+++ src/pages/home/components/aboutUs.jsx
@@ -3,6 +3,7 @@ import im1 from "../../../img/home/good-business-talk-2021-09-01-10-50-33-utc.pn
 import { motion } from "framer-motion";
 import { photoAnim } from "../../../animation";
 import pointBlue from "../../../img/pointBlue.png";
+import { Link } from "react-router-dom";
 
 const AboutUs = () => {
     return (
@@ -13,7 +14,9 @@ const AboutUs = () => {
                         <p >Crece tu negocio con la mejor plataforma de <b>Facturación en Línea</b> y <b>Servicios Contables</b></p>
                         <h3 style={{ fontSize: '25px', marginTop: '20px' }}>Hacemos tu contabilidad y te ofrecemos el sistema mas facil de usar, ideal para Pymes y Emprendedores.</h3>
                     </div>
-                    <button className="btnAboutUs">Conoce más de nuestros servicios</button>
+                    <Link to="/servicios-contables">
+                        <button className="btnAboutUs">Conoce más de nuestros servicios</button>
+                    </Link>
                 </div>
                 <div className="description-right">
                     <img src={im1} alt="im1" style={{ width: "85%" }}></img>
