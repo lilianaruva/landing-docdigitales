39cbd60e10942f1142b82ece32150c57c99252d8
Martín Valenzuela <martynvalenz@gmail.com>
2022-01-25 14:17:07 -0700

    developers section

--- src/pages/services/payAdmin/components/secondList.jsx
+++ src/pages/services/payAdmin/components/secondList.jsx
@@ -1,31 +1,44 @@
 import React from 'react';
 import im1 from "../../../../img/services/payAdmin/Screen Shot 2021-12-29 at 13.46.23.png";
+import { MdKeyboardArrowRight } from "react-icons/md";
 
 const SecondList = () => {
     return (
         <>
             <div className="flex lg:columns-2 h-screen items-center" style={{ backgroundColor: "#E7F9FA" }}>
-                <div className="description-left">
-                    <div className="simple-description">
-                        <div className="title">Conoce nuestro servicio de <b>cálculo y timbrado de nómina</b></div>
-                        <div className="description secondList">
-                            <ul>
-                                <li>Cálculos automáticos</li>
-                                <li>Control de vacaciones</li>
-                                <li>Histórico de nominas</li>
-                                <li>Control de todo tu equipo en un solo lugar</li>
-                                <li>Reportes</li>
-                                <li>Validez ante el SAT</li>
-                                <li>Recurrencia de pagos</li>
-                                <li>Dispersión en un solo clic</li>
-                            </ul>
-                        </div>
+                <div className="lg:w-5/12 lg:px-[80px]">
+                    <div className="lg:text-[25px] xl:text-[32px] leading-tight pb-[20px]">Conoce nuestro servicio de <b>cálculo y timbrado de nómina</b></div>
+                    <div className="text-[22px]">
+                        <ul>
+                            <li className='flex items-center py-[5px]'> 
+                                <MdKeyboardArrowRight style={{ fontSize:'22px', color:'#13BFC9' }}/> Cálculos automáticos
+                            </li>
+                            <li className='flex items-center py-[5px]'> 
+                                <MdKeyboardArrowRight style={{ fontSize:'22px', color:'#13BFC9' }}/> Control de vacaciones
+                            </li>
+                            <li className='flex items-center py-[5px]'> 
+                                <MdKeyboardArrowRight style={{ fontSize:'22px', color:'#13BFC9' }}/> Histórico de nominas
+                            </li>
+                            <li className='flex items-center py-[5px]'> 
+                                <MdKeyboardArrowRight style={{ fontSize:'22px', color:'#13BFC9' }}/> Control de todo tu equipo en un solo lugar
+                            </li>
+                            <li className='flex items-center py-[5px]'> 
+                                <MdKeyboardArrowRight style={{ fontSize:'22px', color:'#13BFC9' }}/> Reportes
+                            </li>
+                            <li className='flex items-center py-[5px]'> 
+                                <MdKeyboardArrowRight style={{ fontSize:'22px', color:'#13BFC9' }}/> Validez ante el SAT
+                            </li>
+                            <li className='flex items-center py-[5px]'> 
+                                <MdKeyboardArrowRight style={{ fontSize:'22px', color:'#13BFC9' }}/> Recurrencia de pagos
+                            </li>
+                            <li className='flex items-center py-[5px]'> 
+                                <MdKeyboardArrowRight style={{ fontSize:'22px', color:'#13BFC9' }}/> Dispersión en un solo clic
+                            </li>
+                        </ul>
                     </div>
                 </div>
-                <div className="description-right w-1/2">
-                    <div className="image justify-end">
-                        <img src={im1} alt="im12"></img>
-                    </div>
+                <div className="lg:w-7/12">
+                    <img src={im1} alt="im12" style={{ right:'-20%', float:'right'}}></img>
                 </div>
             </div>
         </>
